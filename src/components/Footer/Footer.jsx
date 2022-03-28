e44709b36cd47134c25bc0b460c722b04bc2db4b
Michael Bashford <mikebashford@gmail.com>
2022-03-27 21:42:45 -0500

    Added footer content

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,48 @@
 import React from "react";
 import "./footer.css";
+import { BsTwitter, BsGithub, BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Mike Bashford
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://twitter.com">
+          <BsTwitter />
+        </a>
+        <a href="https://github.com">
+          <BsGithub />
+        </a>
+        <a href="https://linkedin.com">
+          <BsLinkedin />
+        </a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Michael Bashford. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
