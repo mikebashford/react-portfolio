a18b87e31c089a5ef4e511ed3ddb6e99690a3f81
Michael Bashford <mikebashford@gmail.com>
2022-03-27 21:23:48 -0500

    Added contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,70 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./contact.css";
+import { MdOutlineMailOutline } from "react-icons/md";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_3oifiac",
+        "template_wog2xx7",
+        form.current,
+        "OagJR9OXXyCYPwn5E"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineMailOutline className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>mikebashford@gmail.com</h5>
+            <a href="mailto:mikebashford@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
